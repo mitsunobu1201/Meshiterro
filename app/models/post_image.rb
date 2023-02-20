b2f8ad12bb90398d9ab37cd0ee2808ce68c4b5f1
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-02-20 11:40:01 +0000

    [Update]アソシエーション,routes設定変更

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,6 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
+  
+  #belongs_to は、PostImage モデルから user_id に関連付けられていて、User モデルを参照することができる。
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         #1:Nの1側が削除されたとき、N側を全て削除するという意味。ここではUserが削除された時に、そのUserが投稿したPostImageが全て削除されるとなる。
+         #has_many = たくさん持っているという意味。
+         has_many :post_images, dependent: :destroy
 end
